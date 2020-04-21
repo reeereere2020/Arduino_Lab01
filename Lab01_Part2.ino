f1484e4218c92523e3c1b2c6194d119c27ca9523
reeereere2020 <61824449+reeereere2020@users.noreply.github.com>
2020-04-21 15:38:44 +0800

    Add files via upload

---
+++ Lab01_Part2.ino
@@ -0,0 +1,24 @@
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  for(int i=0;i<3;i++) {
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(100);
+  }
+  for(int j=0;j<3;j++) {
+    digitalWrite(led, HIGH);
+     delay(300);
+    digitalWrite(led, LOW);
+    delay(100);
+  }
+  for(int k=0;k<3;k++) {
+    digitalWrite(led, HIGH);
+    delay(100);
+  }
+}
